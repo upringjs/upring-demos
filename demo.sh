36400ae9d14d22b40f4bb9e4d2b8d021c385a199
Matteo Collina <hello@matteocollina.com>
2017-03-17 15:28:59 +0100

    Better demo!

--- demo.sh
+++ demo.sh
@@ -8,22 +8,22 @@ export MYIP=`ipconfig getifaddr en0`
 tmux new-session -s upring -n baseswim -d
 tmux send-keys -t upring "export MYIP=$MYIP" C-m
 tmux send-keys -t upring "export PATH=`pwd`/node_modules/.bin:$PATH" C-m
-tmux send-keys -t upring 'baseswim --host $MYIP --port 7979 -v | pino' C-m
+tmux send-keys -t upring 'baseswim --host $MYIP --port 7979 -V | pino' C-m
 tmux new-window -n upring-3042
 tmux send-keys -t upring "export MYIP=$MYIP" C-m
 tmux send-keys -t upring "export PATH=`pwd`/node_modules/.bin:$PATH" C-m
 tmux send-keys -t upring 'sleep 1' C-m
-tmux send-keys -t upring "upring-kv -P 10 -p 3042 $MYIP:7979 -v | pino" C-m
+tmux send-keys -t upring "upring-kv -P 10 -p 3042 $MYIP:7979 -V | pino" C-m
 tmux new-window -n upring-3043
 tmux send-keys -t upring "export MYIP=$MYIP" C-m
 tmux send-keys -t upring "export PATH=`pwd`/node_modules/.bin:$PATH" C-m
 tmux send-keys -t upring 'sleep 1' C-m
-tmux send-keys -t upring "upring-kv -P 10 -p 3043 $MYIP:7979 -v | pino" C-m
+tmux send-keys -t upring "upring-kv -P 10 -p 3043 $MYIP:7979 -V | pino" C-m
 tmux new-window -n upring-3044
 tmux send-keys -t upring "export MYIP=$MYIP" C-m
 tmux send-keys -t upring "export PATH=`pwd`/node_modules/.bin:$PATH" C-m
 tmux send-keys -t upring 'sleep 1' C-m
-tmux send-keys -t upring "upring-kv -P 10 -p 3044 $MYIP:7979 -v | pino" C-m
+tmux send-keys -t upring "upring-kv -P 10 -p 3044 $MYIP:7979 -V | pino" C-m
 tmux new-window -n upring-control
 tmux send-keys -t upring "export MYIP=$MYIP" C-m
 tmux send-keys -t upring "export PATH=`pwd`/node_modules/.bin:$PATH" C-m
